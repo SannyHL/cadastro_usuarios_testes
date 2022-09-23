8ca87a3c0b933a7472bbeced38fc91cbf773851f
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-23 17:13:25 -0300

    Adição de contrutores.

--- api/src/main/java/com/br/projetopessoal/api/model/UserModel.java
+++ api/src/main/java/com/br/projetopessoal/api/model/UserModel.java
@@ -3,10 +3,14 @@ package com.br.projetopessoal.api.model;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class UserModel {
     

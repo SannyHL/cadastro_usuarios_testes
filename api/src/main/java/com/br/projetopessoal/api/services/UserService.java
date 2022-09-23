15ba2944455d38c61c1a2d18fcb18e08e754bc48
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-23 17:13:50 -0300

    Criação de Service.

---
+++ api/src/main/java/com/br/projetopessoal/api/services/UserService.java
@@ -0,0 +1,31 @@
+package com.br.projetopessoal.api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.projetopessoal.api.model.UserModel;
+import com.br.projetopessoal.api.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    public UserModel findById(Integer id) {
+        Optional<UserModel> userOptional = userRepository.findById(id);
+        return userOptional.orElse(null);
+    }
+
+
+
+
+    
+}

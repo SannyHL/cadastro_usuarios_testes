4af97fa2eb7049404bcdf74caeec2dbdcf467571
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-24 13:27:05 -0300

    Criação de Interface.

---
+++ api/src/main/java/com/br/projetopessoal/api/interfaces/UserInterface.java
@@ -0,0 +1,14 @@
+package com.br.projetopessoal.api.interfaces;
+
+import java.util.List;
+
+import com.br.projetopessoal.api.dtos.UserDto;
+import com.br.projetopessoal.api.model.UserModel;
+
+
+public interface UserInterface {
+
+    UserModel findById(Integer id);
+    List<UserModel> findAll();
+    UserModel create(UserDto userDto);
+}

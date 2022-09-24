bcd08ac58b6df7d76e9a635d6a75277659231ff1
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-24 13:25:16 -0300

    Inserção JsonProperty.

--- api/src/main/java/com/br/projetopessoal/api/dtos/UserDto.java
+++ api/src/main/java/com/br/projetopessoal/api/dtos/UserDto.java
@@ -2,6 +2,8 @@ package com.br.projetopessoal.api.dtos;
 
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -13,5 +15,6 @@ public class UserDto {
     @NotBlank
     private String email;
     @NotBlank
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 }

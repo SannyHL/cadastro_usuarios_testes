87df392dfc744bbe202cf6f620e48a848a4ca403
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-26 14:42:22 -0300

    Teste de findAll do UserController.

--- api/src/test/java/com/br/projetopessoal/api/controllers/UserControllerTest.java
+++ api/src/test/java/com/br/projetopessoal/api/controllers/UserControllerTest.java
@@ -6,6 +6,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,7 +16,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.yaml.snakeyaml.events.Event.ID;
 
 import com.br.projetopessoal.api.dtos.UserDto;
 import com.br.projetopessoal.api.model.UserModel;
@@ -55,7 +60,23 @@ public class UserControllerTest {
     }
 
     @Test
-    void testFindAll() {
+    void whenFindAllThenReturnAnListUserDto() {
+        when(service.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDto);
+
+        ResponseEntity<List<UserDto>> response = userController.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDto.class, response.getBody().get(INDEX).getClass());
+
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
 
     }
 

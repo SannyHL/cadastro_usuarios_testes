53fcf3c4a3b19bea3d48083e4da0eae3b51377ca
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-26 16:50:28 -0300

    Teste de DataIntegratyViolationException.

--- api/src/test/java/com/br/projetopessoal/api/exceptions/ResourceExceptionHadlerTest.java
+++ api/src/test/java/com/br/projetopessoal/api/exceptions/ResourceExceptionHadlerTest.java
@@ -1,7 +1,10 @@
 package com.br.projetopessoal.api.exceptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,7 +29,7 @@ public class ResourceExceptionHadlerTest {
 
     @Test
     void whenObjectNotFoundThenReturnAResponseEntity() {
-        ResponseEntity<StandardError> response = exception.ObjectNotFound(new ObjectNotFoundException("Objeto não encontrado"), new MockHttpServletRequest());
+        ResponseEntity<StandardError> response = exception.objectNotFound(new ObjectNotFoundException("Objeto não encontrado"), new MockHttpServletRequest());
 
         assertNotNull(response);
         assertNotNull(response.getBody());
@@ -35,11 +38,23 @@ public class ResourceExceptionHadlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals("Objeto não encontrado", response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
+
 
     }
 
     @Test
-    void testDataIntegratyViolationException() {
+    void whenDataIntegratyViolationExceptionThenRetornResponseEntity() {
+        ResponseEntity<StandardError> response = exception.dataIntegratyViolationException(new DataIntegratyViolationException("Esse email já foi ultlizado, favor informar outro!"), new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Esse email já foi ultlizado, favor informar outro!", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
 
     }
 }
